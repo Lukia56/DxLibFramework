3267c5fc663ac0e5d11289abc64e3b86dd033ad0
Lukia56 <kky56saqqe@gmail.com>
2026-05-29 10:07:03 +0900

    ベクトルのニアリーイコール対応

--- Utility/Vector.h
+++ Utility/Vector.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cassert>
-#include <cmath>
 #include <DxLib.h>
 
 /// <summary>
@@ -24,121 +22,55 @@ struct Vector2
 
 	// 演算子
 
-	constexpr Vector2 operator-() const { return Vector2(-x, -y); }
+	Vector2 operator-() const;
 
-	constexpr Vector2 operator+(const Vector2& v) const { return Vector2(x + v.x, y + v.y); }
-	constexpr Vector2 operator-(const Vector2& v) const { return Vector2(x - v.x, y - v.y); }
-	constexpr Vector2 operator*(const float s) const { return Vector2(x * s, y * s); }
-	constexpr Vector2 operator/(const float s) const
-	{
-		if (s == 0.0f)
-		{
-			assert(false && "Vector2 // 0除算が行われました");
-			return Vector2::Zero;
-		}
+	Vector2 operator+(const Vector2& v) const;
+	Vector2 operator-(const Vector2& v) const;
+	Vector2 operator*(const float s) const;
+	Vector2 operator/(const float s) const;
 
-		return Vector2(x / s, y / s);
-	}
+	Vector2& operator+=(const Vector2& v);
+	Vector2& operator-=(const Vector2& v);
+	Vector2& operator*=(const float s);
+	Vector2& operator/=(const float s);
 
-	constexpr Vector2& operator+=(const Vector2& v)
-	{
-		x += v.x;
-		y += v.y;
+	float operator[](int index) const;
 
-		return *this;
-	}
-	constexpr Vector2& operator-=(const Vector2& v)
-	{
-		x -= v.x;
-		y -= v.y;
-
-		return *this;
-	}
-	constexpr Vector2& operator*=(const float s)
-	{
-		x *= s;
-		y *= s;
-
-		return *this;
-	}
-	constexpr Vector2& operator/=(const float s)
-	{
-		if (s == 0.0f)
-		{
-			assert(false && "Vector2 // 0除算が行われました");
-			return *this;
-		}
-
-		x /= s;
-		y /= s;
-
-		return *this;
-	}
-
-	float operator[](int index) const
-	{
-		return *(&x + index);
-	}
-
-	constexpr bool operator==(const Vector2& v) const { return x == v.x && y == v.y; }
-	constexpr bool operator!=(const Vector2& v) const { return x != v.x || y != v.y; }
+	bool operator==(const Vector2& v) const;
+	bool operator!=(const Vector2& v) const;
 
 	// ヘルパー関数
 
 	/// <summary>
 	/// 内積を計算する
 	/// </summary>
-	constexpr float Dot(const Vector2& v) const
-	{
-		return (x * v.x) + (y * v.y);
-	}
+	float Dot(const Vector2& v) const;
 
 	/// <summary>
 	/// 外積を計算する
 	/// </summary>
-	constexpr float Cross(const Vector2& v) const
-	{
-		return (x * v.y) - (y * v.x);
-	}
+	float Cross(const Vector2& v) const;
 
 	/// <summary>
 	/// ベクトルの長さを取得する
 	/// 処理が重いため非推奨
 	/// </summary>
-	inline float GetLength() const
-	{
-		return std::sqrt(GetSqLength());
-	}
+	float GetLength() const;
 
 	/// <summary>
 	/// ベクトルの長さの2乗を取得する
 	/// </summary>
-	constexpr float GetSqLength() const
-	{
-		return x * x + y * y;
-	}
+	float GetSqLength() const;
 
 	/// <summary>
 	/// 正規化したベクトルを取得する
 	/// </summary>
-	constexpr Vector2 GetNormalize() const
-	{
-		if (*this == Vector2::Zero)
-		{
-			assert(false && "Vector2 // 0除算が行われました");
-			return Vector2::Zero;
-		}
-
-		return *this / GetLength();
-	}
+	Vector2 GetNormalize() const;
 
 	/// <summary>
 	/// DxLibのベクトルに変換する
 	/// </summary>
-	inline VECTOR GetAsDxLibVector() const
-	{
-		return VGet(x, y, 0.0f);
-	}
+	VECTOR GetAsDxLibVector() const;
 
 	// 定数
 
@@ -170,128 +102,55 @@ struct Vector3
 
 	// 演算子
 
-	constexpr Vector3 operator-() const { return Vector3(-x, -y, -z); }
+	Vector3 operator-() const;
 
-	constexpr Vector3 operator+(const Vector3& v) const { return Vector3(x + v.x, y + v.y, z + v.z); }
-	constexpr Vector3 operator-(const Vector3& v) const { return Vector3(x - v.x, y - v.y, z - v.z); }
-	constexpr Vector3 operator*(const float s) const { return Vector3(x * s, y * s, z * s); }
-	constexpr Vector3 operator/(const float s) const
-	{
-		if (s == 0.0f)
-		{
-			assert(false && "Vector3 // 0除算が行われました");
-			return Vector3::Zero;
-		}
+	Vector3 operator+(const Vector3& v) const;
+	Vector3 operator-(const Vector3& v) const;
+	Vector3 operator*(const float s) const;
+	Vector3 operator/(const float s) const;
 
-		return Vector3(x / s, y / s, z / s);
-	}
+	Vector3& operator+=(const Vector3& v);
+	Vector3& operator-=(const Vector3& v);
+	Vector3& operator*=(const float s);
+	Vector3& operator/=(const float s);
 
-	constexpr Vector3& operator+=(const Vector3& v)
-	{
-		x += v.x;
-		y += v.y;
-		z += v.z;
+	float operator[](int index) const;
 
-		return *this;
-	}
-	constexpr Vector3& operator-=(const Vector3& v)
-	{
-		x -= v.x;
-		y -= v.y;
-		z -= v.z;
-
-		return *this;
-	}
-	constexpr Vector3& operator*=(const float s)
-	{
-		x *= s;
-		y *= s;
-		z *= s;
-
-		return *this;
-	}
-	constexpr Vector3& operator/=(const float s)
-	{
-		if (s == 0.0f)
-		{
-			assert(false && "Vector3 // 0除算が行われました");
-			return *this;
-		}
-
-		x /= s;
-		y /= s;
-		z /= s;
-
-		return *this;
-	}
-
-	float operator[](int index) const
-	{
-		return *(&x + index);
-	}
-
-	constexpr bool operator==(const Vector3& v) const { return x == v.x && y == v.y && z == v.z; }
-	constexpr bool operator!=(const Vector3& v) const { return x != v.x || y != v.y || z != v.z; }
+	bool operator==(const Vector3& v) const;
+	bool operator!=(const Vector3& v) const;
 
 	// ヘルパー関数
 
 	/// <summary>
 	/// 内積を計算する
 	/// </summary>
-	constexpr float Dot(const Vector3& v) const
-	{
-		return (x * v.x) + (y * v.y) + (z * v.z);
-	}
+	float Dot(const Vector3& v) const;
 
 	/// <summary>
 	/// 外積を計算する
 	/// </summary>
-	constexpr Vector3 Cross(const Vector3& v) const
-	{
-		return Vector3(
-			y * v.z - z * v.y,
-			z * v.x - x * v.z,
-			x * v.y - y * v.x);
-	}
+	Vector3 Cross(const Vector3& v) const;
 
 	/// <summary>
 	/// ベクトルの長さを取得する
 	/// 処理が重いため非推奨
 	/// </summary>
-	inline float GetLength() const
-	{
-		return std::sqrt(GetSqLength());
-	}
+	float GetLength() const;
 
 	/// <summary>
 	/// ベクトルの長さの2乗を取得する
 	/// </summary>
-	constexpr float GetSqLength() const
-	{
-		return x * x + y * y + z * z;
-	}
+	float GetSqLength() const;
 
 	/// <summary>
 	/// 正規化したベクトルを取得する
 	/// </summary>
-	constexpr Vector3 GetNormalize() const
-	{
-		if (*this == Vector3::Zero)
-		{
-			assert(false && "Vector3 // 0除算が行われました");
-			return Vector3::Zero;
-		}
-
-		return *this / GetLength();
-	}
+	Vector3 GetNormalize() const;
 
 	/// <summary>
 	/// DxLibのベクトルに変換する
 	/// </summary>
-	inline VECTOR GetAsDxLibVector() const
-	{
-		return VGet(x, y, z);
-	}
+	VECTOR GetAsDxLibVector() const;
 
 	// 定数
 
