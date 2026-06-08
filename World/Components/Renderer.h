7be0593bbf1faffd766e578d237d9c2ad76cf8ff
Lukia56 <kky56saqqe@gmail.com>
2026-06-08 20:56:09 +0900

    グラフィックリソースのレンダリングの基底クラス追加

---
+++ World/Components/Renderer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include "System/ResourceManager.h"
+
+class GameObject;
+class Resource;
+
+/// <summary>
+/// グラフィックリソースの描画管理の基底クラス
+/// </summary>
+template<class T>
+requires std::derived_from<T, Resource>
+class Renderer
+{
+public:
+
+	Renderer(GameObject* owner) : mResource(nullptr), mOwner(owner) {}
+
+	virtual ~Renderer()
+	{
+		mResource = nullptr;
+		mOwner = nullptr;
+	}
+
+	virtual void Load(const std::string& filePath)
+	{
+		mResource = ResourceManager::GetInstance().GetResource<T>(filePath);
+	}
+
+	virtual void Draw() = 0;
+
+protected:
+
+	Resource* mResource;
+
+	GameObject* mOwner;
+};
