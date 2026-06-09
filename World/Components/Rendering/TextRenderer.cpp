b47002a8a947251c81fe9555b94efb6beea9dc10
Lukia56 <kky56saqqe@gmail.com>
2026-06-09 10:54:55 +0900

    色とスケーリングを適用可能に変更

--- World/Components/Rendering/TextRenderer.cpp
+++ World/Components/Rendering/TextRenderer.cpp
@@ -6,7 +6,9 @@
 
 TextRenderer::TextRenderer(GameObject* owner) :
 	Renderer(owner),
-	mText("")
+	mText(""),
+	mColor(Color::white),
+	mScalingFlag(false)
 {
 }
 
@@ -15,13 +17,26 @@ void TextRenderer::Draw()
 	if (!mOwner) return;
 
 	const VECTOR pos = mOwner->GetTransform().GetWorldPosition().GetAsDxLibVector();
+	
+	// 必要な場合のみワールドスケールを計算して取得
+	VECTOR scale = VGet(0.0f, 0.0f, 0.0f);
+	if (mScalingFlag)
+		scale = mOwner->GetTransform().GetWorldScale().GetAsDxLibVector();
 
+	// フォントが読み込まれていたらフォントを使用してテキストを表示する
 	if (mResource)
 	{
-		DrawStringToHandle(pos.x, pos.y, mText.c_str(), Color::white.GetAsHexRGB(), mResource->GetHandle());
+		if (mScalingFlag)
+			DrawExtendStringFToHandle(pos.x, pos.y, scale.x, scale.y, mText.c_str(), mColor.GetAsHexRGB(), mResource->GetHandle());
+		else
+			DrawStringFToHandle(pos.x, pos.y, mText.c_str(), mColor.GetAsHexRGB(), mResource->GetHandle());
 	}
+	// フォントが読み込まれていなかったらデフォルトのフォントを使用してテキストを表示する
 	else
 	{
-		DrawString(pos.x, pos.y, mText.c_str(), Color::white.GetAsHexRGB());
+		if (mScalingFlag)
+			DrawExtendStringF(pos.x, pos.y, scale.x, scale.y, mText.c_str(), mColor.GetAsHexRGB());
+		else
+			DrawStringF(pos.x, pos.y, mText.c_str(), mColor.GetAsHexRGB());
 	}
 }
--- World/Components/Rendering/TextRenderer.h
+++ World/Components/Rendering/TextRenderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Renderer.h"
+#include "Utility/Color.h"
 
 class TextRenderer : public Renderer<Font>
 {
@@ -11,7 +12,18 @@ public:
 
 	void Draw() override;
 
+	/// <summary>
+	/// 表示するテキストを変更する
+	/// </summary>
 	void SetText(const std::string& text) { mText = text; }
+	/// <summary>
+	/// テキストの色を設定する
+	/// </summary>
+	void SetColor(const Color& color) { mColor = color; }
+	/// <summary>
+	/// スケーリングを適用するかのフラグを設定する
+	/// </summary>
+	void SetScalingFlag(const bool flag) { mScalingFlag = flag; }
 
 private:
 
@@ -19,4 +31,14 @@ private:
 	/// 表示するテキスト
 	/// </summary>
 	std::string mText;
+
+	/// <summary>
+	/// テキストの色
+	/// </summary>
+	Color mColor;
+
+	/// <summary>
+	/// スケーリングをするかどうか
+	/// </summary>
+	bool mScalingFlag;
 };
